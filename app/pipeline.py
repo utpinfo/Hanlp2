9ac658aa43c573ed95c209eaced76b0d464ae3e8
UTPInfo <56543943+utpinfo@users.noreply.github.com>
2025-09-08 17:41:47 +0800

    Generate BIO from Oracle11g

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,10 +1,20 @@
 import hanlp
+
 """
 Service層
 """
 tokenizer = hanlp.load('FINE_ELECTRA_SMALL_ZH')
+
+# 加载用户自定义词典
+custom_dict = {}
+with open("./data/dict/dict1.txt", "r", encoding="utf-8") as f:
+    for line in f:
+        word, tag = line.strip().split()
+        custom_dict[word] = tag  # 例如: 借样 NN
+tokenizer.dict_force = custom_dict
+
 pos_tagger = hanlp.load(hanlp.pretrained.pos.CTB9_POS_ELECTRA_SMALL)
-ner_model = hanlp.load('data/model/ner/product_bert')
+ner_model = hanlp.load('./data/model/ner/product_bert')
 
 # 建立 pipeline
 pipeline = hanlp.pipeline() \
@@ -12,13 +22,20 @@ pipeline = hanlp.pipeline() \
     .append(pos_tagger, input_key='tok', output_key='pos') \
     .append(ner_model, input_key='tok', output_key='ner')
 
+
 def ner_predict(text: str):
     res = pipeline(text)
     tokens = res['tok']
     pos_tags = res['pos']
     ner_tags = ['O'] * len(tokens)
 
-    # 將實體 span 轉成逐 token NER
+    # 覆盖 POS：如果 token 在自定义词典里，就强制用词典里的 POS
+    for i, tok in enumerate(tokens):
+        if tok in custom_dict:
+            pos_tags[i] = custom_dict[tok]
+
+
+    # 將實體(NER) span 轉成逐 token NER
     for entity_text, entity_type, start, end in res['ner']:
         for i in range(start, end):
             ner_tags[i] = entity_type
