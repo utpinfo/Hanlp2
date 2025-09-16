6ccb221a5a7fb942a3758923de8b4397e66d8894
UTPInfo <56543943+utpinfo@users.noreply.github.com>
2025-09-16 15:45:54 +0800

    Update README

--- scripts/test2.py
+++ scripts/test2.py
@@ -2,6 +2,6 @@ from app.pipeline import ner_predict
 
 # 範例
 if __name__ == "__main__":
-    text = "查詢楊馮凱库存"
+    text = "查詢上海倉海裡嗯库存"
     ner_output = ner_predict(text)  # 先拿逐 token 結果
     print(ner_output)
